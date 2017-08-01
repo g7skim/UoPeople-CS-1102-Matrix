bf44cdab67cde03ecd7fe41864506677d2517f4a
g7skim <jamuday@me.com>
2017-08-02 00:27:16 +0300

    Add some comments to the code

--- Matrix/src/matrix/Matrix.java
+++ Matrix/src/matrix/Matrix.java
@@ -4,9 +4,18 @@ package matrix;
  * @author Eugene Vereshchagin (g7skim)
  */
 public class Matrix {
+    /*
+     * Declare the variables for numbers of rows and columns and assign them to zeros
+     */
     static int numberOfRows = 0;
     static int numberOfColumns  = 0;
     
+    /*
+     * Create a two dimension array for our 3x3 matrix m with the values from the
+     * learning guide Unit 7
+     * After it, find a size of the matrix m using a standart .length method
+     * and print it for the User
+     */
     public static void main(String[] args) {
         int [][] m =
         {
@@ -22,12 +31,18 @@ public class Matrix {
     System.out.println("Number of Columns: " +m[0].length);
     System.out.println();
         
+    /*
+     * If matrix m is square (number of rows is equale to number of columns)
+     * then we can calculate the secondary diagonal further. If the matrix is
+     * not square, then User must receive a message "This is not a square matrix"
+     */
+    
     if (numberOfRows == numberOfColumns) {    
         int secondaryDiagonalSum = 0;
         int size = m.length -1;
         
         for(int i = 0; i<m.length; i++){
-            secondaryDiagonalSum+= m[i][size-i];
+            secondaryDiagonalSum += m[i][size-i];
         }
             
         System.out.println("This is a square matrix.");
